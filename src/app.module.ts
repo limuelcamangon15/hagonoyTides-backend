e389a12d84a2f0049b439499c4550ffcad6e32a4
Limuel <limuelmartincamangon@gmail.com>
2025-12-16 15:42:55 +0800

    setup mongodbbbbbb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatsModule } from './chats/chats.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ChatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URI!),
+    ChatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
