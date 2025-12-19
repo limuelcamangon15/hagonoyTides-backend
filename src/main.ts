0692dd92d99ee37f7839588ed9383276898c8f09
Limuel <limuelmartincamangon@gmail.com>
2025-12-19 16:51:53 +0800

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://hagonoy-tides.vercel.app'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
