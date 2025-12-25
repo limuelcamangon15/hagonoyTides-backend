62aadc667284b47be3d8b5bc08e0be3b00ad9357
Limuel <limuelmartincamangon@gmail.com>
2025-12-25 20:22:41 +0800

    added ping (health)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ChatsModule } from './chats/chats.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { WeatherModule } from './weather/weather.module';
+import { AppController } from './app.controller';
 import 'dotenv/config';
 
 @Module({
@@ -12,5 +13,6 @@ import 'dotenv/config';
     ChatsModule,
     WeatherModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
