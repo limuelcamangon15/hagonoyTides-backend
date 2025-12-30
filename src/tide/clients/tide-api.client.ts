fb9ed0368c0085b3bbd8451b99e46cd999d7c50e
Limuel <limuelmartincamangon@gmail.com>
2025-12-30 21:15:56 +0800

    adjusted coordinates var

--- src/tide/clients/tide-api.client.ts
+++ src/tide/clients/tide-api.client.ts
@@ -11,16 +11,16 @@ export class TideApiClient {
   ) {}
 
   async fetchTide(startDate: string) {
-    const HAGONOY_LAT = 14.5188;
-    const HAGONOY_LON = 120.758;
+    const MANILABAY_LAT = 14.5188;
+    const MANILABAY_LON = 120.758;
     const HIGHTIDE_OFFSET = 0.51;
     const LOWTIDE_OFFSET = 0.52;
 
     const response = await lastValueFrom(
       this.httpService.get('https://www.worldtides.info/api/v3', {
         params: {
-          lat: HAGONOY_LAT,
-          lon: HAGONOY_LON,
+          lat: MANILABAY_LAT,
+          lon: MANILABAY_LON,
           date: startDate,
           days: 7,
           extremes: true,
